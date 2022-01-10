0e8471e73321b39f709c8a315900817042eaea7e
crra <8239189+crra@users.noreply.github.com>
2022-01-10 23:44:28 +0100

    Generic slice methods:
    - partition a slice by a partition function and wrap the result
      so that the original order of the slice is preserved. Can later
      be used in the platform `sort.Slice` function.
    
    - use the partition result (or any type) and combine two slices.
      Any difference of the elements is used from the second slice
      rather than the first slice. The intended use case is to have
      a slice of files from a directory (readDir) and a slice of
      expliciltly set files via the command line. The explicit files
      shall be removed from the directory files.

--- slice/slice.go
+++ slice/slice.go
@@ -73,3 +73,65 @@ func Map[T any, U any](list []T, fn func(T) U) []U {
 
 	return values
 }
+
+type PartitionResult[T Stringer] struct {
+	Element       T
+	OriginalIndex int
+}
+
+func (p *PartitionResult[T]) String() string {
+	return p.Element.String()
+}
+
+type PartitionStrResult struct {
+	Element       string
+	OriginalIndex int
+}
+
+func (p *PartitionStrResult) String() string {
+	return p.Element
+}
+
+func PartitionStr[T Stringer, K comparable](haystack []T, fn func(T) K) map[K][]PartitionResult[T] {
+	p := make(map[K][]PartitionResult[T])
+
+	for i, e := range haystack {
+		key := fn(e)
+		p[key] = append(p[key], PartitionResult[T]{Element: e, OriginalIndex: i})
+	}
+
+	return p
+}
+
+// UnionButIntersectionFromA takes two slices (a and b) and combines the slices (incl. duplicates from 'b').
+// If an element in 'a' is also present in 'b' (intersection) the value from 'b' (incl. duplicates) is used instead.
+func UnionButIntersectionFromB[T any, K comparable](a, b []T, fn func(T) K) []T {
+	union := make([]T, 0, len(a)+len(b))
+	bElComps := make([]K, len(b))
+
+	// 'b' has priority over 'a' so keep all the elements (incl. duplicates)
+	for i, bb := range b {
+		union = append(union, bb)
+		// convert elements from 'b' as comparable: O(n)
+		bElComps[i] = fn(bb)
+	}
+
+	// convert elements from 'a' as comparable: O(m)
+	for _, aa := range a {
+		aElComp := fn(aa)
+		isAlsoInB := false
+
+		for _, bElComp := range bElComps {
+			if aElComp == bElComp {
+				isAlsoInB = true
+				break
+			}
+		}
+
+		if !isAlsoInB {
+			union = append(union, aa)
+		}
+	}
+
+	return union
+}
